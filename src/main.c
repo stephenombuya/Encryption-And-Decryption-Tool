3f8374567cddeab6fe10a966469395b73405b329
Stephen Ombuya <129269603+stephenombuya@users.noreply.github.com>
2025-01-02 18:07:42 +0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -12,26 +12,25 @@ void caesarDecrypt(char *text);
 void vigenereEncrypt(char *text, const char *key);
 void vigenereDecrypt(char *text, const char *key);
 void xorEncrypt(char *text, const char *key);
-void generateKey(char *key, int length);
+void generateRandomKey(char *key, int length);
+void promptForKey(char *key, int maxLength, const char *method);
 
 // Caesar cipher functions
 void caesarEncrypt(char *text) {
-    for(int i = 0; text[i] != '\0'; i++) {
-        if(text[i] >= 'a' && text[i] <= 'z') {
+    for (int i = 0; text[i] != '\0'; i++) {
+        if (text[i] >= 'a' && text[i] <= 'z') {
             text[i] = 'a' + (text[i] - 'a' + CAESAR_KEY) % 26;
-        }
-        else if(text[i] >= 'A' && text[i] <= 'Z') {
+        } else if (text[i] >= 'A' && text[i] <= 'Z') {
             text[i] = 'A' + (text[i] - 'A' + CAESAR_KEY) % 26;
         }
     }
 }
 
 void caesarDecrypt(char *text) {
-    for(int i = 0; text[i] != '\0'; i++) {
-        if(text[i] >= 'a' && text[i] <= 'z') {
+    for (int i = 0; text[i] != '\0'; i++) {
+        if (text[i] >= 'a' && text[i] <= 'z') {
             text[i] = 'a' + (text[i] - 'a' - CAESAR_KEY + 26) % 26;
-        }
-        else if(text[i] >= 'A' && text[i] <= 'Z') {
+        } else if (text[i] >= 'A' && text[i] <= 'Z') {
             text[i] = 'A' + (text[i] - 'A' - CAESAR_KEY + 26) % 26;
         }
     }
@@ -40,13 +39,12 @@ void caesarDecrypt(char *text) {
 // Vigenère cipher functions
 void vigenereEncrypt(char *text, const char *key) {
     int keyLen = strlen(key);
-    for(int i = 0, j = 0; text[i] != '\0'; i++) {
-        if(text[i] >= 'a' && text[i] <= 'z') {
+    for (int i = 0, j = 0; text[i] != '\0'; i++) {
+        if (text[i] >= 'a' && text[i] <= 'z') {
             text[i] = 'a' + (text[i] - 'a' + (key[j % keyLen] - 'a')) % 26;
             j++;
-        }
-        else if(text[i] >= 'A' && text[i] <= 'Z') {
-            text[i] = 'A' + (text[i] - 'A' + (key[j % keyLen] - 'a')) % 26;
+        } else if (text[i] >= 'A' && text[i] <= 'Z') {
+            text[i] = 'A' + (text[i] - 'A' + (key[j % keyLen] - 'A')) % 26;
             j++;
         }
     }
@@ -54,13 +52,12 @@ void vigenereEncrypt(char *text, const char *key) {
 
 void vigenereDecrypt(char *text, const char *key) {
     int keyLen = strlen(key);
-    for(int i = 0, j = 0; text[i] != '\0'; i++) {
-        if(text[i] >= 'a' && text[i] <= 'z') {
+    for (int i = 0, j = 0; text[i] != '\0'; i++) {
+        if (text[i] >= 'a' && text[i] <= 'z') {
             text[i] = 'a' + (text[i] - 'a' - (key[j % keyLen] - 'a') + 26) % 26;
             j++;
-        }
-        else if(text[i] >= 'A' && text[i] <= 'Z') {
-            text[i] = 'A' + (text[i] - 'A' - (key[j % keyLen] - 'a') + 26) % 26;
+        } else if (text[i] >= 'A' && text[i] <= 'Z') {
+            text[i] = 'A' + (text[i] - 'A' - (key[j % keyLen] - 'A') + 26) % 26;
             j++;
         }
     }
@@ -69,24 +66,31 @@ void vigenereDecrypt(char *text, const char *key) {
 // XOR encryption/decryption
 void xorEncrypt(char *text, const char *key) {
     int keyLen = strlen(key);
-    for(int i = 0; text[i] != '\0'; i++) {
-        text[i] = text[i] ^ key[i % keyLen];
+    for (int i = 0; text[i] != '\0'; i++) {
+        text[i] ^= key[i % keyLen];
     }
 }
 
 // Key generation
-void generateKey(char *key, int length) {
-    const char charset[] = "abcdefghijklmnopqrstuvwxyz";
-    for(int i = 0; i < length; i++) {
-        key[i] = charset[rand() % 26];
+void generateRandomKey(char *key, int length) {
+    const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    for (int i = 0; i < length; i++) {
+        key[i] = charset[rand() % strlen(charset)];
     }
     key[length] = '\0';
+}
+
+// Prompt for custom key
+void promptForKey(char *key, int maxLength, const char *method) {
+    printf("Enter a key for %s encryption (max %d characters): ", method, maxLength);
+    fgets(key, maxLength, stdin);
+    key[strcspn(key, "\n")] = 0; // Remove newline
 }
 
 int main() {
     char text[MAX_LENGTH], key[MAX_LENGTH];
     int choice, method;
-    
+
     srand(time(NULL));
 
     printf("Select encryption method:\n");
@@ -106,24 +110,22 @@ int main() {
     fgets(text, MAX_LENGTH, stdin);
     text[strcspn(text, "\n")] = 0;
 
-    switch(method) {
+    switch (method) {
         case 1:
-            if(choice == 1) caesarEncrypt(text);
-            else if(choice == 2) caesarDecrypt(text);
+            if (choice == 1) caesarEncrypt(text);
+            else if (choice == 2) caesarDecrypt(text);
             printf("Result: %s\n", text);
             break;
 
         case 2:
-            generateKey(key, 8);
-            printf("Generated key: %s\n", key);
-            if(choice == 1) vigenereEncrypt(text, key);
-            else if(choice == 2) vigenereDecrypt(text, key);
+            promptForKey(key, MAX_LENGTH, "Vigenère");
+            if (choice == 1) vigenereEncrypt(text, key);
+            else if (choice == 2) vigenereDecrypt(text, key);
             printf("Result: %s\n", text);
             break;
 
         case 3:
-            generateKey(key, 16);
-            printf("Generated key: %s\n", key);
+            promptForKey(key, MAX_LENGTH, "XOR");
             xorEncrypt(text, key);
             printf("Result: %s\n", text);
             break;
